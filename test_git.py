fdbef8801c0191a2737d61a52ccc104e14d54a8f
Bart <artembo14545@icloud.com>
2025-08-05 14:51:31 +0300

    Изменили файл, что бы посмотреть как отображаются изменения

--- test_git.py
+++ test_git.py
@@ -9,7 +9,26 @@ class Stack:
         return self.items.pop()
 
     def peek(self):
-        return self.items[len(self.items)-1]
+        return self.items[len(self.items) - 1]
 
     def size(self):
         return len(self.items)
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items) - 1]
+
+    def size(self):
+        return len(self.items)
+
+
+
